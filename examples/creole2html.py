e3313576abc3f6ed7db656209d195a720eca063f
Soenke J. Peters <peters__git@opcenter.de>
2017-01-16 14:59:48 +0100

    Revert to previous

--- examples/creole2html.py
+++ examples/creole2html.py
@@ -7,6 +7,13 @@ from creole import Parser
 from creole.html_emitter import HtmlEmitter
 
 if __name__=="__main__":
-    document = Parser(unicode(sys.stdin.read(), 'utf-8', 'ignore')).parse()
-    sys.stdout.write(HtmlEmitter(document).emit().encode('utf-8', 'ignore'))
+    try:
+        document = Parser(unicode(sys.stdin.read(), 'utf-8', 'ignore')).parse()
+    except NameError:
+        document = Parser(sys.stdin.read()).parse()
+
+    try:
+        sys.stdout.write(HtmlEmitter(document).emit().encode('utf-8', 'ignore'))
+    except TypeError:
+        sys.stdout.write(HtmlEmitter(document).emit())
 
